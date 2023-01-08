9a0c49847f82684cade615ea9a8f8925e07848ad
tyange <usun16@gmail.com>
2023-01-08 21:33:49 +0900

    add route

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"pian-gin/routes"
 )
 
 func main() {
 	r := gin.Default()
 	routes.Routes(r)
-	r.Run(":8081")
+	log.Fatal(r.Run(":8081"))
 }
